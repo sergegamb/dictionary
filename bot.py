a0adaec7b3ef5d944c17961fb99ccec118052ae2
Sergey Gambaryan <serge_gamb@yahoo.com>
2025-02-11 17:08:43 +0300

    Add words representation with pagination

--- bot.py
+++ bot.py
@@ -81,11 +81,87 @@ async def no(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     return ConversationHandler.END
 
 
-app = ApplicationBuilder().token(os.getenv("TOKEN")).build()
+ITEMS_PER_PAGE = 5
 
-app.add_handler(CommandHandler("hello", hello))
+
+def get_words(start_index: int, end_index: int) -> list:
+    words_list = list(DICTIONRY)
+    words_list.reverse()
+    return words_list[start_index:end_index]
+
+
+def words_keyboard(words):
+    keyboard = []
+    for word in words:
+        keyboard.append([
+            InlineKeyboardButton(
+                word,
+                callback_data=f'word@{word}'
+            )
+        ])
+    return keyboard
+
+
+next_page_button = InlineKeyboardButton(
+    'next >>',
+    callback_data='next_page'
+)
+
+
+previous_page_button = InlineKeyboardButton(
+    '<< previous',
+    callback_data='previous_page'
+)
+
+
+async def send_page(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    page = context.user_data.get("page", 1)
+    start_index = (page - 1) * ITEMS_PER_PAGE
+    end_index = start_index + ITEMS_PER_PAGE
+    current_page_words = get_words(start_index, end_index)
+
+    keyboard = words_keyboard(current_page_words)
+    pagination_keys = []
+    if page > 1:
+        pagination_keys.append(previous_page_button)
+    if end_index < len(DICTIONRY):
+        pagination_keys.append(next_page_button)
+    keyboard.append(pagination_keys)
+
+    message = 'Слова'
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    if update.callback_query:
+        await update.callback_query.edit_message_text(text=message, reply_markup=reply_markup)
+    else:
+        await update.message.reply_text(text=message, reply_markup=reply_markup)
+
+
+async def words(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    context.user_data['page'] = 1
+    await send_page(update, context)
+
+
+async def next_page(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    context.user_data['page'] += 1
+    await send_page(update, context)
+
+
+async def previous_page(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    context.user_data['page'] -= 1
+    await send_page(update, context)
+
+
+async def word(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    word = update.callback_query.data.split('@').pop()
+    await update.callback_query.answer(DICTIONRY[word])
+
+
+app = ApplicationBuilder().token(os.getenv('TOKEN')).build()
+
+app.add_handler(CommandHandler('hello', hello))
 app.add_handler(ConversationHandler(
-    [CommandHandler("add", add)],
+    [CommandHandler('add', add)],
     {
         TRANSLATION: [MessageHandler(filters.TEXT & ~filters.COMMAND, translation)],
         CONFIRM: [MessageHandler(filters.TEXT & ~filters.COMMAND, confirm)],
@@ -93,5 +169,9 @@ app.add_handler(ConversationHandler(
     },
     []
 ))
+app.add_handler(CommandHandler("words", words))
+app.add_handler(CallbackQueryHandler(word, 'word'))
+app.add_handler(CallbackQueryHandler(next_page, 'next_page'))
+app.add_handler(CallbackQueryHandler(previous_page, 'previous_page'))
 
 app.run_polling()
